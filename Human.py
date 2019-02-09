ed96310c7adbccde453835fcde870cbf8039f5e7
taramic42 <39064626+taramic42@users.noreply.github.com>
2019-02-09 13:53:28 -0500

    Human logic fully implemented

--- Human.py
+++ Human.py
@@ -5,23 +5,37 @@ Created on Thu Feb  7 13:48:59 2019
 @author: m_taran
 """
 
-import DoubleCard.Player as Player
-import DoubleCard.Grid as Grid
+import Player as Player
+import Grid as Grid
 
 class Human(Player.Player):
     
     def __init__(self):
-        super()
+        super(Human,self).__init__()
     
     #Should detemine if player should perform regular or recycling move
     #Grid should not be modified here, it's only a parameter in the function to allow the AI to see the board once it's implemented
     def turnStart(self, board : Grid.Grid):
-        print("Turn has started")
+        print("\nTurn has started")
+        print("Here's the current board:\n")
+        board.display()
+        if(self.hand > 0):
+            print("\nRegular play initiated")
+            self.hand -= 1
+            return self.playFromHand(board)
+        else:
+            print("\nRecycle play initiated")
+            return self.recycleAndPlay(board)
+            
     
     #Prompt user for regular play command and ensure it's of the correct format
     def playFromHand(self, board : Grid.Grid):
         print("Playing from hand")
+        command = input("Where would you like to play?")
+        return command
         
     #Prompt user for recycle play command and ensure it's of the correct format
     def recycleAndPlay(self, board : Grid.Grid):
         print("Recycling and playing")
+        command = input("Where would you like to play?")
+        return command
