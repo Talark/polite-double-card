2f7f041d984d6b4a4e6c1caf856d8657d7296c3a
mtl-tony <40874800+mtl-tony@users.noreply.github.com>
2019-02-10 17:21:09 -0500

    Update Grid.py
    Added in the otherHalfCoordinates, otherHalfInfo and a new dictionary for detemringing the values at every orientation.  Most of playCard should function now, just needs to make sure that it works properly as there is one section on the use of updating the dictionarrie's value which is untested and needs to be checked.

--- Grid.py
+++ Grid.py
@@ -131,9 +131,18 @@ class Grid:
                         "12": HalfCard.HalfCard(0,0,"")
                             }
                 }
-
-    def buildCard(self, command):
-        angle = command[0]
+        #Used for looking up based on orientation
+        #Returns [Color,Dot]
+        self.cardInfo = {
+                "1": [2,2],
+                "2": [1,1],
+                "3": [1,1],
+                "4": [2,2],
+                "5": [2,1],
+                "6": [1,2],
+                "7": [1,2],
+                "8": [2,1]
+                }
         
     #We need to change the input string to a list
     def inputToList(self, inputString): 
@@ -147,6 +156,23 @@ class Grid:
         commandFormated = self.inputToList(command)
         print("Putting",command,"in grid if legal")
         isLegal = self.moveIsLegal(commandFormated)
+        if(isLegal):
+            return(True)
+            #First half piece refers to the coordinate that we received, while second half piece is the coordinate of the piece
+            #that we interpolated from the orientation
+            firstHalfPieceChar =commandFormated[2]
+            firstHalfPieceNum = commandFormated[3]
+            orientation = commandFormated[1]
+            neighborCoordinates = self.otherHalfCoordinates(orientation, firstHalfPieceChar, firstHalfPieceNum)
+            secondHalfPieceChar = neighborCoordinates[0]
+            secondHalfPieceNum = neighborCoordinates[1]
+            firstHalfInfo = self.cardInfo[orientation]
+            secondHalfInfo= self.otherHalfInfo(firstHalfInfo)
+            #PROOF CHECK THIS
+            self.board[firstHalfPieceChar][firstHalfPieceNum].setValues(firstHalfInfo[0],firstHalfInfo[1],"")
+            self.board[secondHalfPieceChar][secondHalfPieceNum].setValues(secondHalfInfo[0],secondHalfInfo[1],"")            
+        else:
+            return(False)
     
     #same as above, but has an additional legality check (ensure command targets 2 halves of 1 piece and piece is not under others)
     #can modify recycle command into a play command afterwards (just an implementation option if you want to do it that way)
@@ -180,6 +206,23 @@ class Grid:
         return( self.board[indexLetter1][indexNum1] == self.board[indexLetter2][indexNum2].neighbor)
                        
         print("Checking if move is legal")
+        
+       #Method for finding the coordinates of the other half of a tile.
+    #Returns a list with [Character,Number] values
+    #HELPER
+    def otherHalfCoordinates(self,orientation, indexLetter, indexNumber):
+        if(int(orientation) % 2 == 1):
+            indexLetterNeighbor = chr(ord(indexLetter) + 1)
+            indexNumberNeighbor = indexNumber
+        else:
+            indexLetterNeighbor = indexLetter
+            indexNumberNeighbor = chr(ord(indexNumber) + 1)
+        return([indexLetterNeighbor, indexNumberNeighbor])
+        
+    #Type list needs to be card info with first part being 
+    def ohterHalfInfo(self,cardinfo):
+        return(-1*np.subtract(cardinfo,[3,3]))
+        
         
     #This method should receive coordinates from which it needs to radiate out from to check for a win
     #In general this method must check for wins from 2 origin points since cards have 2 halves that can be part of a 4-set
