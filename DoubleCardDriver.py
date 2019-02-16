bf6b4a2a061844015ab6d41210aeaed7640fd6ce
mtl-tony <40874800+mtl-tony@users.noreply.github.com>
2019-02-15 19:38:48 -0500

    Adding in the commented out test of Recycling

--- DoubleCardDriver.py
+++ DoubleCardDriver.py
@@ -10,6 +10,32 @@ import GameController as GC
 
 test = GC.GameController()
 
+#test.gameBoard.playCard("0 5 A 1")
+#test.gameBoard.playCard("0 5 A 2")
+#test.gameBoard.playCard("0 5 A 3")
+#test.gameBoard.playCard("0 5 C 1")
+#test.gameBoard.playCard("0 5 C 2")
+#test.gameBoard.playCard("0 5 C 3")
+#test.gameBoard.playCard("0 5 E 1")
+#test.gameBoard.playCard("0 5 E 2")
+#test.gameBoard.playCard("0 5 E 3")
+#test.gameBoard.playCard("0 5 G 1")
+#test.gameBoard.playCard("0 5 G 2")
+#test.gameBoard.playCard("0 5 G 3")
+
+#test.gameBoard.playCard("0 7 A 4")
+#test.gameBoard.playCard("0 7 A 5")
+#test.gameBoard.playCard("0 7 A 6")
+#test.gameBoard.playCard("0 7 C 4")
+#test.gameBoard.playCard("0 7 C 5")
+#test.gameBoard.playCard("0 7 C 6")
+#test.gameBoard.playCard("0 7 E 4")
+#test.gameBoard.playCard("0 7 E 5")
+#test.gameBoard.playCard("0 7 E 6")
+#test.gameBoard.playCard("0 7 G 4")
+#test.gameBoard.playCard("0 7 G 5")
+#test.gameBoard.playCard("0 7 G 6")
+#test.turn = 60
 test.runHumanVSHuman()
 
 #Prompt user for game type to run
