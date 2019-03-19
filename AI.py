f7653ed928b230586346099d63dc1e6be64792dc
taramic42 <39064626+taramic42@users.noreply.github.com>
2019-03-19 14:49:47 -0400

    Whoops thanks Tony
    Didn't see notification from teacher for some reason Thanks for setting me straight Tony! :D
    Ai spends at most 6 seconds searching a tree depth of 2 beyond root which complies with current game requirements

--- AI.py
+++ AI.py
@@ -31,13 +31,10 @@ class AI(Player.Player):
         self.depth = value
     
     def turnStart(self, board : Grid.Grid, opponentHand = 0):
-        if(opponentHand == 0):
-            self.depth = 1
         
         if(self.hand>0):
             self.playFromHand(board, opponentHand)
         else:
-            self.depth = 1
             self.recycleAndPlay(board, opponentHand)
         
         nody = Node.Node(self.hand,opponentHand,self.playType, self.playType,1-self.playType,board, self.depth)
